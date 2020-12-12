f03435a89a54386dbbf78b072c464593d8003de2
Robert Rayburn <robert.rayburn@core10.io>
2020-12-11 21:10:36 -0600

    added in gif generator for code chagnes

--- Day11/part2.py
+++ Day11/part2.py
@@ -1,3 +1,6 @@
+#import os
+#import glob
+#from PIL import Image, ImageDraw
 with open('data.txt', 'r') as f: prior_seats = [x for x in f.read().split('\n') if x]
 
 new_seats = prior_seats[:]
@@ -14,7 +17,7 @@ def check_for_fill(grid, row, col, row_diff, col_diff):
     return check_for_fill(grid, row + row_diff, col + col_diff, row_diff, col_diff)
 
 chaos = True
-
+#inc = 0
 while chaos:
     for i in range(len(prior_seats)):
         for j in range(len(prior_seats[i])):
@@ -55,19 +58,19 @@ while chaos:
                     adj_seats_filled += check_for_fill(prior_seats, i - 1, j + 1, -1, 1)
                     adj_seats_filled += check_for_fill(prior_seats, i, j + 1, 0, 1)
                     adj_seats_filled += check_for_fill(prior_seats, i + 1, j, 1, 0)
-		    adj_seats_filled += check_for_fill(prior_seats, i + 1, j + 1, 1, 1)
+                    adj_seats_filled += check_for_fill(prior_seats, i + 1, j + 1, 1, 1)
                 elif j == len(prior_seats[i]) - 1:
                     adj_seats_filled += check_for_fill(prior_seats, i - 1, j, -1, 0)
-		    adj_seats_filled += check_for_fill(prior_seats, i - 1, j - 1, -1, -1)
+                    adj_seats_filled += check_for_fill(prior_seats, i - 1, j - 1, -1, -1)
                     adj_seats_filled += check_for_fill(prior_seats, i, j - 1, 0, -1)
                     adj_seats_filled += check_for_fill(prior_seats, i + 1, j - 1, 1, -1)
                     adj_seats_filled += check_for_fill(prior_seats, i + 1, j, 1, 0)
                 else:
                     adj_seats_filled += check_for_fill(prior_seats, i - 1 , j, -1, 0)
-		    adj_seats_filled += check_for_fill(prior_seats, i - 1, j + 1, -1, 1)
+                    adj_seats_filled += check_for_fill(prior_seats, i - 1, j + 1, -1, 1)
                     adj_seats_filled += check_for_fill(prior_seats, i, j + 1, 0, 1)
                     adj_seats_filled += check_for_fill(prior_seats, i + 1, j, 1, 0)
-		    adj_seats_filled += check_for_fill(prior_seats, i + 1, j + 1, 1, 1)
+                    adj_seats_filled += check_for_fill(prior_seats, i + 1, j + 1, 1, 1)
                     adj_seats_filled += check_for_fill(prior_seats, i + 1, j - 1, 1, -1)
                     adj_seats_filled += check_for_fill(prior_seats, i - 1, j - 1, -1, -1)
                     adj_seats_filled += check_for_fill(prior_seats, i, j - 1, 0, -1)
@@ -78,10 +81,15 @@ while chaos:
                 new_seats[i] = new_seats[i][:j] + '#' + new_seats[i][j+1:]
             else:
                 new_seats[i] = new_seats[i][:j] + prior_seats[i][j] + new_seats[i][j+1:]
+    #img = Image.new('RGB', (700,1600), color=(255,255,255))
+    #d = ImageDraw.Draw(img)
+    #d.text((20, 20), '\n'.join(n for n in new_seats), fill=(0,0,0))
+    #img.save('text_' + str(inc) + '.png')
     chaos = prior_seats[:] != new_seats[:]
     temp = prior_seats[:]
     prior_seats = new_seats[:]
     new_seats = temp[:]
+    #inc += 1
 
 num_of_seats = 0
 for i in prior_seats:
@@ -89,3 +97,13 @@ for i in prior_seats:
         if j == '#':
             num_of_seats += 1
 print(num_of_seats)
+
+
+#gif_name = 'day_11'
+#file_list = glob.glob('*.png')
+#list.sort(file_list, key=lambda x: int(x.split('_')[1].split('.png')[0]))
+
+#with open('image_list.txt', 'w') as file:
+#    for item in file_list:
+#        file.write('%s\n' % item)
+#os.system('convert @image_list.txt {}.gif'.format(gif_name))
