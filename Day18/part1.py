3426f160ddd004fc80902f04efc0755dbee6c2df
Robert Rayburn <robert.rayburn@core10.io>
2020-12-18 22:20:40 -0600

    removed prints and commented code

--- Day18/part1.py
+++ Day18/part1.py
@@ -9,10 +9,7 @@ def solve_equation(equation):
         equation = equation.split(' ')
         rmv = int(equation[-1])
         rmo = equation[-2]
-        print(rmv)
-        print(rmo)
         equation = ' '.join(equation[:-2])
-        print(equation)
         if rmo == '*':
             return solve_equation(equation) * rmv
         else:
@@ -30,16 +27,9 @@ with open('data.txt', 'r') as f:
     equations = [x for x in f.read().split('\n') if x]
 total_sum = 0
 paren_pattern = re.compile('\\([0-9*+\\s]*\\)')
-# paren_pattern = re.compile('\\(\\d+\\s[*+]\\s\\d+\\)')
-# paren_pattern = re.compile('(\\((\\d+\\s[*+]\\s)+\\d+\\))')
-# \(\d\s[*+]\s+\d\)|\((\d\s[*+]\s)+\d\)
-# print(equations)
 for equation in equations:
     while '(' in equation:
         paren_equations = paren_pattern.findall(equation)
-        print(paren_equations)
-        # for pe in paren_equations:
-        #     print(pe[1:-1])
         paren_solutions = []
         if len(paren_equations) > 0:
             paren_solutions = solve_paren(paren_equations)
