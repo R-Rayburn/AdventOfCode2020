e1fd03d618c8f9229467da2a2b0a1c7c0e4f477e
Robert Rayburn <robcrayb@ut.utm.edu>
2022-12-02 14:58:53 +0000

    2015 - JSAbacusFramework.io - Part 2

--- 2015/Day12/js_abacus_framework.py
+++ 2015/Day12/js_abacus_framework.py
@@ -22,14 +22,22 @@ def total_num_sum(data):
     return sum(data)
 
 
-def total_num_sum_exclude_red(data):
-    data = json.loads(data)
-    print(data)
-    # data = str(data)
-    # return total_num_sum(data)
-
 
 print('Part 1')
 print(f'Data: {total_num_sum(data_json)}')
+
+
+def total_num_sum_exclude_red(data, total_value):
+    if isinstance(data, int):
+        return data + total_value
+    if isinstance(data, dict) and 'red' not in data.values():
+        for value in data.values():
+            total_value = total_num_sum_exclude_red(value, total_value)
+    elif isinstance(data, list):
+        for value in data:
+            total_value = total_num_sum_exclude_red(value, total_value)
+    return total_value
+
+
 print('Part 2')
-print(f'Data: {total_num_sum_exclude_red(data_json)}')
+print(f'Data: {total_num_sum_exclude_red(json.loads(data_json), 0)}')
