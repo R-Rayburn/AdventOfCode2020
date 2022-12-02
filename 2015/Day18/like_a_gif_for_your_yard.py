c012af6b5497a9afb8739f86034ee502c71a747c
Robert Rayburn <robcrayb@ut.utm.edu>
2022-12-02 22:34:51 +0000

    initial files for 2015 | Day18

---
+++ 2015/Day18/like_a_gif_for_your_yard.py
@@ -0,0 +1,11 @@
+with open('data.txt', 'r') as file:
+    containers = file.read().split('\n')
+
+with open('test.txt', 'r') as test_file:
+    test_containers = test_file.read().split('\n')
+
+def switch_lights(current_state, new_state):
+    for i in range(len(current_state)):
+        for j in range(len(current_state[0])):
+            if current_state[i][j] == '.':
+                
