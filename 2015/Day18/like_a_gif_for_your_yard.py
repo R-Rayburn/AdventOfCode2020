7426d176574ca97f121ca9a52fd3bd4650b5efe9
Robert Rayburn <robcrayb@ut.utm.edu>
2022-12-04 04:52:46 +0000

    added temp pass

--- 2015/Day18/like_a_gif_for_your_yard.py
+++ 2015/Day18/like_a_gif_for_your_yard.py
@@ -8,4 +8,4 @@ def switch_lights(current_state, new_state):
     for i in range(len(current_state)):
         for j in range(len(current_state[0])):
             if current_state[i][j] == '.':
-                
+                pass
